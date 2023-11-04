59b08ccde63f4c6a8d1ff05a460c751f7689e654
Yeasir-Hossain <yeasir06@gmail.com>
2023-11-04 18:08:32 +0600

    fix:checked css for the image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -8,32 +8,29 @@ import { removeSelectedImage, selectImage } from '../redux/reducers/imageReducer
  * @returns react component
  */
 export const Image = ({ image }) => {
-    // state to checked
-    // condition for checking
-    // checkbox
-    // hover class
-    // checked class
     const dispatch = useDispatch();
     const [checked, setChecked] = useState(false)
-    const { images, selectedImages } = useSelector((state) => ({
-        images: state.imageStore.images,
+    const { selectedImages } = useSelector((state) => ({
         selectedImages: state.imageStore.selectedImages
     }), shallowEqual)
+
+    //set the checkbox true if the image is selected
     useEffect(() => {
         setChecked(selectedImages.find(i => i.id === image.id) ? true : false);
     }, [selectedImages, image.id])
+
     return (
         <div className='relative group first:row-span-2 first:col-span-2 first:w-full first:h-full lg:w-44 lg:h-44 border-2 border-gray-200 rounded-lg'>
             <input
-                onClick={() => checked ? dispatch(removeSelectedImage(image)) : dispatch(selectImage(image))}
+                onChange={() => checked ? dispatch(removeSelectedImage(image)) : dispatch(selectImage(image))}
                 type="checkbox"
                 name={`${image.id}`}
                 id={`${image.id}`}
                 checked={checked}
-                className={`absolute z-50 top-[5px] left-[5px] w-5 h-5 ${checked ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'} transition-opacity duration-500 ease-in-out`}
+                className={`absolute z-50 top-[20px] left-[20px] w-5 h-5 ${checked ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'} transition-opacity duration-500 ease-in-out`}
             />
             <label htmlFor={`${image.id}`}>
-                <div className='absolute w-full h-full z-20 top-0 left-0 opacity-40 group-hover:backdrop-blur-sm bg-transparent group-hover:bg-black transition-all duration-500 ease-in-out'></div>
+                <div className={`absolute w-full h-full z-20 top-0 left-0 opacity-40 group-hover:backdrop-blur-sm ${checked ? 'bg-white' : 'bg-transparent'} group-hover:bg-black transition-all duration-500 ease-in-out`}></div>
                 <img src={image?.path} className='relative w-full h-full' />
             </label>
         </div>
