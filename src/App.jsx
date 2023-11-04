359017efc8a6839edfc4acab967f7de04f2a6f36
Yeasir-Hossain <yeasir06@gmail.com>
2023-11-05 04:18:19 +0600

    feat:drag and drop without animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { ImageGallery } from "./features/ImageGallery";
-import Test from "./test";
 
 function App() {
   return (
     <main className="container max-w-5xl min-h-screen flex justify-center items-center py-5">
       <ImageGallery />
-      {/* <Test /> */}
     </main>
   );
 }
--- src/components/ImagesContainer.jsx
+++ src/components/ImagesContainer.jsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 import { Image } from './Image';
 import { ImageUploader } from './ImageUploader';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
-import { useDispatch } from 'react-redux';
-import { setValue } from '../redux/reducers/imageReducer';
-
 /**
  * This component renders the image
  * @returns react component
@@ -14,6 +10,11 @@ export const ImagesContainer = () => {
     const { images } = useSelector((state) => ({
         images: state.imageStore.images
     }), shallowEqual);
+
+    const noop = function () { };
+    useEffect(() => {
+
+    }, []);
 
     return (
         <div className={`grid ${images.length < 1 ? 'grid-cols-1' : 'sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'} gap-4 px-5 py-5`}>
